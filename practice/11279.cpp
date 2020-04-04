18fbc9b02584c60dd216008e551d064e6df7c6b2
SunKyu <leesunkeuy@naver.com>
2020-04-04 20:48:15 +0900

    practice

---
+++ practice/11279.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int N;
+
+int main(void){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin>>N;
+    priority_queue<int, vector<int>, less<int> > pq;
+    int a;
+    for (int i = 0; i < N; i++)
+    {
+        cin>>a;
+        if(a==0){
+            if(pq.empty()){
+                cout<<0<<'\n';
+                continue;
+            }
+            cout<<pq.top()<<'\n';
+            pq.pop();
+            continue;
+        }
+        pq.push(a);
+    }
+   return 0; 
+}
